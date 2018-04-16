72ecb8e0f4528155b633b1469261cf26a3e09e81
PickyPunk <corentin.lente@viacesi.fr>
2018-04-16 14:01:06 +0200

    Add files via upload

--- boutique.php
+++ boutique.php
@@ -1,8 +1,9 @@
+
 <head>
 	<meta charset="utf-8" />
-	<link rel="stylesheet" href="Css/style.css"/>
+	<link rel="stylesheet" href="style.css"/>
 	<link rel="stylesheet" href="bootstrap/css/bootstrap.css"/>
-	<link rel="stylesheet" href="Css/formulaire.css">
+
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 	<title>Boutique</title>
 </head>
@@ -10,7 +11,7 @@
 	<header>
 
 	 
-		<img src="Image/exia.png">
+		
 
 		<?php 
 
@@ -25,20 +26,31 @@
       <h1 class="display-4">Boutique</h1>
       <p class="lead">Bienvenue dans la boutique du BDE, qui contient des articles ( snack et boisson ) et des goodies !</p>
     </div>  
+<?php
+    $bdd = new PDO('mysql:host=localhost;dbname=phpweb','root','');
 
-       
-          <!-- début pour un article -->
-          <div class="row">
+
+
+
+    $boutique = $bdd->query('SELECT * FROM article');
+
+
+   while ( $donnees = $boutique->fetch())
+    { 
+
+      ?>
+  
+<div class="row">
                     <!-- debut a copier -->
             <div class="col-lg-4">
               <div class="card h-100">
-                <a href="#"><img class="card-img-top" src="Image/coca.png" alt=""></a> <!-- image etc..-->
+                <a href="#"><img class="card-img-top" src="Image/<?php echo $donnees['Url'] ?>" alt=""></a> <!-- image etc..-->
                 <div class="card-body">
                   <h4 class="card-title">
-                    Cola <!-- Titre de l'article -->
+                    <?php  echo $donnees['Article'] ?> <!-- Titre de l'article -->
                   </h4>
-                  <h5>1000€</h5> <!-- prix de l'article -->
-                  <p class="card-text">Soda a base de cola</p>  <!-- description de l'article -->
+                  <h5><?php  echo $donnees['Prix'] ?> €</h5> <!-- prix de l'article -->
+                  <p class="card-text"><?php echo $donnees['Description'] ?></p>  <!-- description de l'article -->
                 </div>
                 <div class="card-footer"><div class="btn-group" role="group">
                     <div class="form-group">
@@ -57,137 +69,19 @@
             </div>
           </div>
 
+<?php
+    }
+
+?>
+
+
+
+       
+          <!-- début pour un article -->
+          
+
             <!-- fin a copier -->
-                   <div class="col-lg-4">
-              <div class="card h-100">
-                <a href="#"><img class="card-img-top" src="Image/icetea.png" alt=""></a> <!-- image etc..-->
-                <div class="card-body">
-                  <h4 class="card-title">
-                    Cola <!-- Titre de l'article -->
-                  </h4>
-                  <h5>1000€</h5> <!-- prix de l'article -->
-                  <p class="card-text">Soda a base de cola</p>  <!-- description de l'article -->
-                </div>
-                <div class="card-footer"><div class="btn-group" role="group">
-                    <div class="form-group">
-                    <label for="exampleFormControlSelect1">Quantité</label> <!--la quantité de l'article -->
-                            <select class="form-control" id="exampleFormControlSelect1">
-                                 <option>1</option>
-                                 <option>2</option>
-                                 <option>3</option>
-                                 <option>4</option>
-                                 <option>5</option>
-                            </select>
-                  </div>
-                 <button type="button" class="btn btn-danger active">Achéte !</button>
-          </div>
-          </div>
-            </div>
-          </div>
-                 <div class="col-lg-4">
-              <div class="card h-100">
-                <a href="#"><img class="card-img-top" src="Image/fanta.png" alt=""></a> <!-- image etc..-->
-                <div class="card-body">
-                  <h4 class="card-title">
-                    Cola <!-- Titre de l'article -->
-                  </h4>
-                  <h5>1000€</h5> <!-- prix de l'article -->
-                  <p class="card-text">Soda a base de cola</p>  <!-- description de l'article -->
-                </div>
-                <div class="card-footer"><div class="btn-group" role="group">
-                    <div class="form-group">
-                    <label for="exampleFormControlSelect1">Quantité</label> <!--la quantité de l'article -->
-                            <select class="form-control" id="exampleFormControlSelect1">
-                                 <option>1</option>
-                                 <option>2</option>
-                                 <option>3</option>
-                                 <option>4</option>
-                                 <option>5</option>
-                            </select>
-                  </div>
-                 <button type="button" class="btn btn-danger active">Achéte !</button>
-          </div>
-          </div>
-            </div>
-          </div>
-                 <div class="col-lg-4">
-              <div class="card h-100">
-                <a href="#"><img class="card-img-top" src="Image/coca.png" alt=""></a> <!-- image etc..-->
-                <div class="card-body">
-                  <h4 class="card-title">
-                    Cola <!-- Titre de l'article -->
-                  </h4>
-                  <h5>1000€</h5> <!-- prix de l'article -->
-                  <p class="card-text">Soda a base de cola</p>  <!-- description de l'article -->
-                </div>
-                <div class="card-footer"><div class="btn-group" role="group">
-                    <div class="form-group">
-                    <label for="exampleFormControlSelect1">Quantité</label> <!--la quantité de l'article -->
-                            <select class="form-control" id="exampleFormControlSelect1">
-                                 <option>1</option>
-                                 <option>2</option>
-                                 <option>3</option>
-                                 <option>4</option>
-                                 <option>5</option>
-                            </select>
-                  </div>
-                 <button type="button" class="btn btn-danger active">Achéte !</button>
-          </div>
-          </div>
-            </div>
-          </div>
-                 <div class="col-lg-4">
-              <div class="card h-100">
-                <a href="#"><img class="card-img-top" src="Image/coca.png" alt=""></a> <!-- image etc..-->
-                <div class="card-body">
-                  <h4 class="card-title">
-                    Cola <!-- Titre de l'article -->
-                  </h4>
-                  <h5>1000€</h5> <!-- prix de l'article -->
-                  <p class="card-text">Soda a base de cola</p>  <!-- description de l'article -->
-                </div>
-                <div class="card-footer"><div class="btn-group" role="group">
-                    <div class="form-group">
-                    <label for="exampleFormControlSelect1">Quantité</label> <!--la quantité de l'article -->
-                            <select class="form-control" id="exampleFormControlSelect1">
-                                 <option>1</option>
-                                 <option>2</option>
-                                 <option>3</option>
-                                 <option>4</option>
-                                 <option>5</option>
-                            </select>
-                  </div>
-                 <button type="button" class="btn btn-danger active">Achéte !</button>
-          </div>
-          </div>
-            </div>
-          </div>
-                 <div class="col-lg-4">
-              <div class="card h-100">
-                <a href="#"><img class="card-img-top" src="Image/coca.png" alt=""></a> <!-- image etc..-->
-                <div class="card-body">
-                  <h4 class="card-title">
-                    Cola <!-- Titre de l'article -->
-                  </h4>
-                  <h5>1000€</h5> <!-- prix de l'article -->
-                  <p class="card-text">Soda a base de cola</p>  <!-- description de l'article -->
-                </div>
-                <div class="card-footer"><div class="btn-group" role="group">
-                    <div class="form-group">
-                    <label for="exampleFormControlSelect1">Quantité</label> <!--la quantité de l'article -->
-                            <select class="form-control" id="exampleFormControlSelect1">
-                                 <option>1</option>
-                                 <option>2</option>
-                                 <option>3</option>
-                                 <option>4</option>
-                                 <option>5</option>
-                            </select>
-                  </div>
-                 <button type="button" class="btn btn-danger active">Achéte !</button>
-          </div>
-          </div>
-            </div>
-          </div>
+                  
 
    
     </div>
