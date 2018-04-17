7455dc49737c6d1ef41c3052eb5d6170d4d5fc6d
PickyPunk <corentin.lente@viacesi.fr>
2018-04-17 09:25:28 +0200

    Add files via upload

--- ProjetsWeb/boutique.php
+++ ProjetsWeb/boutique.php
@@ -1,25 +1,24 @@
 <?php  
-	session_start();
+  session_start();
     if (!isset($_SESSION['Nom'])) {
-   		echo "connecte toi connard";
+      echo "connecte toi connard";
     } 
     else{
-    	
-    	
+      
+      
     }
-
 ?>
-
 <head>
 	<meta charset="utf-8" />
 	<link rel="stylesheet" href="Css/style.css"/>
 	<link rel="stylesheet" href="bootstrap/css/bootstrap.css"/>
-	<link rel="stylesheet" href="Css/formulaire.css">
+
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 	<title>Boutique</title>
 </head>
 <body>
 	<header>
+
 
 	 
 		
@@ -31,12 +30,145 @@
 
 		?>
 
+	<article>
+		
+		 <div class="pricing-header px-3 py-3 pt-md-5 pb-md-4 mx-auto text-center">
+      <h1 class="display-4">Boutique</h1>
+      <p class="lead">Bienvenue dans la boutique du BDE, qui contient des articles ( snack et boisson ) et des goodies !</p>
+    </div>  
+
+
+
+          <div id="plop">
+
+       
+
+          </div> 
+
+<?php
+    $bdd = new PDO('mysql:host=localhost;dbname=phpweb','root','');
+
+
+
+
+    $boutique = $bdd->query('SELECT * FROM article');
+
+
+   while ( $donnees = $boutique->fetch())
+    { 
+
+      ?>
+  
+<div class="row">
+                    <!-- debut a copier -->
+            <div class="col-lg-4">
+              <div class="card h-100">
+                <a href="#"><img class="card-img-top" src="Image/<?php echo $donnees['Url'] ?>" alt=""></a> <!-- image etc..-->
+                <div class="card-body">
+                  <h4 class="card-title">
+                    <?php  echo $donnees['Article'] ?> <!-- Titre de l'article -->
+                  </h4>
+                  <h5><?php  echo $donnees['Prix'] ?> €</h5> <!-- prix de l'article -->
+                  <p class="card-text"><?php echo $donnees['Description'] ?></p>  <!-- description de l'article -->
+                </div>
+                <div class="card-footer"><div class="btn-group" role="group">
+                    <div class="form-group">
+                    <label for="exampleFormControlSelect1">Quantité</label>
+                     <!--la quantité de l'article -->
+                    
+                            <select  name="site" class="form-control" id="exampleFormControlSelect1">
+                                 <option value="1">1</option>
+                                 <option value="2">2</option>
+                                 <option value="3">3</option>
+                                 <option value="4">4</option>
+                                 <option value="5">5</option>
+                            </select>
+
+                  </div>
+                 <button type="button" id="salut" class="btn btn-danger active">Achéte !</button>
+
+          </div>
+          </div>
+            </div>
+          </div>
+
+
+      
+
+
+<script type="text/javascript"  src="ajax.js"></script>
+<script type="text/javascript">
+var plop  = document.getElementById('plop'); 
+var ajouts= document.querySelectorAll("button");
+var options= document.querySelectorAll("option");
+
+
+for(var i =0; i<options.length; i++)
+
+{
+  var option = options[i];
+
+
+
+}
+
+for(var i =0; i<ajouts.length; i++)
+
+{
+  var ajout = ajouts[i];
+
+
+  ajout.addEventListener('click',function(e){
+
+
+
+   var httpRequest = getXMLHttpRequest();
+    httpRequest.onreadystatechange = function () {
+     
+      if (httpRequest.readyState ===4){
+
+
+        alert('ploop');
+       
+       plop.innerHTML = httpRequest.responseText;
+  }
+    }
+        
+     
+  
+
+httpRequest.open('GET','ajoutpanier.php?produitID=<?php  echo $donnees['ID'] ?>&id=<?php  echo $_SESSION['id']?>' ,true);
+//httpRequest.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");  
+
+
+httpRequest.send('');
+  });
+
+
+
+}
+
+
+</script>
+
+
+
+<?php
+    }
+
+?>
+
+          
+   
+    </div>
+
+
+
+
+</article>
+
 
 	</header>
-
-
-
-
 	
 	<footer>
 		<?php
--- ProjetsWeb/header.php
+++ ProjetsWeb/header.php
@@ -1,43 +1,62 @@
-		<img src="Image/exia.png">
+    <img src="Image/exia.png">
 <nav> 
 
-			<div class="head">
-    		<div class="acc">
-    			
-    		<a <?php if(isset($_SESSION['Nom'])){?>  href="accueil.php" <?php } else{?> href ="index.php" <?php  } ?> class="btn btn-primary btn-danger active"><span class="glyphicon glyphicon-home"></span> Accueil</a>
+            <div class="head">
+            <div class="acc">
+                
+            <a <?php if(isset($_SESSION['Nom']))
 
-			<a href="boutique.php" class="btn btn-primary btn-danger active"><span class="glyphicon glyphicon-usd"></span> Boutique</a>
-			
-			<a href="evenement.php" class="btn btn-primary btn-danger active"><span class="glyphicon glyphicon-calendar"></span> Evenement</a>
-			
-			<a href="boiteaidee.php" class="btn btn-primary btn-danger active"><span class="glyphicon glyphicon-thumbs-up"></span> Boite à idée</a>
+            {
+              ?>   
 
-		</div>
-			
-			<?php 
-			if(isset($_SESSION['Nom'])){?>
-				<div class="inscr">
-					<a  href="deconnexion.php" class="btn btn-danger active">Déconnexion</a>
-			</div>
+             href="accueil.php" <?php 
+             }
+              else
+            {
+                ?> href ="index.php" <?php 
 
-    		<?php
-    			}
+             } 
+             ?> 
+             class="btn btn-primary btn-danger active"><span class="glyphicon glyphicon-home"></span> Accueil</a>
 
-    		else
-    			{
-    				?>
-				<div class="inscr">
-					<a  href="inscription.php" class="btn btn-danger active">Inscription</a>
-					<a href="connexion.php" class="btn btn-danger active">Connexion</a>
-			</div>
-    				<?php
-    			}
-    			
-    			?>
-			
-			
-		</div>
+            <a href="boutique.php" class="btn btn-primary btn-danger active"><span class="glyphicon glyphicon-usd"></span> Boutique</a>
+            
+            <a href="evenement.php" class="btn btn-primary btn-danger active"><span class="glyphicon glyphicon-calendar"></span> Evenement</a>
+            
+            <a href="boiteaidee.php" class="btn btn-primary btn-danger active"><span class="glyphicon glyphicon-thumbs-up"></span> Boite à idée</a>
+
+        </div>
+            
+            <?php 
+            if(isset($_SESSION['Nom']))
+                {
+                    ?>
+                <div class="inscr">
+                <a  href="panier.php" class="btn btn-danger active">Panier</a>
+                    <a  href="deconnexion.php" class="btn btn-danger active">Déconnexion</a>
 
 
 
-    	</nav>
+
+            </div>
+
+            <?php
+                }
+            else
+                {
+                    ?>
+                <div class="inscr">
+                    <a  href="inscription.php" class="btn btn-danger active">Inscription</a>
+                    <a href="connexion.php" class="btn btn-danger active">Connexion</a>
+            </div>
+                    <?php
+                }
+                
+                ?>
+            
+            
+        </div>
+
+
+
+</nav>
