a984a75a5de1efe5f9787d7fb01bbe53103f642a
AAxx3 <yoamon51@live.fr>
2018-04-17 09:37:21 +0200

    Add files via upload

--- header.php
+++ header.php
@@ -1,20 +1,52 @@
-<nav> 
+ <nav> 
+ 
 			<div class="head">
     		<div class="acc">
-    		<a href="index.php" class="btn btn-primary btn-danger active"><span class="glyphicon glyphicon-home"></span> Accueil</a>
-			<a href="boutique.php" class="btn btn-primary btn-danger active"><span class="glyphicon glyphicon-usd"></span> Boutique</a>
+
+    			<nav class="navbar navbar-light bg-light">
+  <form class="form-inline">
+    
+ 
+    			
+    		<a <?php if(isset($_SESSION['Nom'])){?>  href="accueil.php" <?php } else{?> href ="index.php" <?php  } ?> class="btn btn-primary btn-danger active"><span class="glyphicon glyphicon-home"></span> Accueil</a>
+
+			<a href="boutique.php" class="btn btn-primary btn-danger active"><span class="glyphicon glyphicon-usd"></span> Boutique</a>			
 			<a href="evenement.php" class="btn btn-primary btn-danger active"><span class="glyphicon glyphicon-calendar"></span> Evenement</a>
+			
 			<a href="boiteaidee.php" class="btn btn-primary btn-danger active"><span class="glyphicon glyphicon-thumbs-up"></span> Boite à idée</a>
+
+			<a href="boiteaidee.php" class="btn btn-primary btn-danger active"><span class="glyphicon glyphicon-shopping-cart"></span> <!-- nombre d'article dans le panier --> Panier </a>
+			<input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+    <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+ </form>
+</nav>
 		</div>
 			
-			
-			<div class="inscr">
-			<a  href="inscription.php" class="btn btn-danger active">Inscription</a>
-			<a href="connexion.php" class="btn btn-danger active">Connexion</a>
+			<?php 
+			if(isset($_SESSION['Nom'])){?>
+				<div class="inscr">
+					<a  href="deconnexion.php" class="btn btn-danger active">Déconnexion</a>
 			</div>
+
+    		<?php
+    			}
+
+    		else
+    			{
+    				?>
+				<div class="inscr">
+					<a  href="inscription.php" class="btn btn-danger active">Inscription</a>
+					<a href="connexion.php" class="btn btn-danger active">Connexion</a>
+			</div>
+    				<?php
+    			}
+    			
+    			?>
+			
 			
 		</div>
 
 
 
-    	</nav>
+   	</nav> 
+ 
