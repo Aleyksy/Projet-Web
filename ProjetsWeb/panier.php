8f913d70daefe8706c1188e94665673989930abd
AAxx3 <yoamon51@live.fr>
2018-04-18 16:49:41 +0200

    Add files via upload

--- ProjetsWeb/panier.php
+++ ProjetsWeb/panier.php
@@ -14,7 +14,7 @@
 <html lang="fr">
 <head>
 	<meta charset="utf-8" />
-	<link rel="stylesheet" href="style.css"/>
+	<link rel="stylesheet" href="Css/style.css"/>
 	 <link rel="stylesheet" href="bootstrap/css/bootstrap.css"/>
 	 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 	<title>Acceuil</title>
