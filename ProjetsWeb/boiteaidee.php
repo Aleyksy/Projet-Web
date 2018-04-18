01b5ada602d3a1f4c9351a3b784512b22ec4395e
AAxx3 <yoamon51@live.fr>
2018-04-18 14:52:40 +0200

    Add files via upload

--- ProjetsWeb/boiteaidee.php
+++ ProjetsWeb/boiteaidee.php
@@ -2,7 +2,7 @@
 <html lang="fr">
 <head>
 	<meta charset="utf-8" />
-	<link rel="stylesheet" href="Css/boiteaidee_style.css"/>
+	<link rel="stylesheet" href="Css/style.css"/>
 	 <link rel="stylesheet" href="bootstrap/css/bootstrap.css"/>
 	<title>Acceuil</title>
 </head>
@@ -25,30 +25,34 @@
 
 		while ($Reader = $requete->fetch() ) {
 	?>
-			<div class="Idea">
+	<div class = "row">
 
-				<div class="Idea_ID_Date">
+		<div class=" col-lg-8 col-lg-push-2">
+			<div class="card text-center">
+
+				<div class="card-header" style="color: white; background: #A52A2A;">
 
 	    			<p class="Idea_ID"> ID : <?php echo $Reader["ID"]; ?> </p>
 	    			<p class="Idea_Date">Date : <?php echo $Reader["Date_Soumission"]; ?> </p>
 
 	    		</div>
 
-	    		<div>
+	    		<div class="card-body" style="margin-bottom: 3%;">
 
-	    			<p class="Idea_Objet"> Objet : <?php echo $Reader["Objet"]; ?>  </p>
-					<p class="Idea_Description">Description : <?php echo $Reader["Description"]; ?> </p>
+	    			<h3 class="card-title"> Objet : <?php echo $Reader["Objet"]; ?>  </h3>
+					<p class="card-text">Description : <?php echo $Reader["Description"]; ?> </p>
+
+					<button type="button" class="btn btn-outline-light"> 5 <!-- compteur --><span class="glyphicon glyphicon-thumbs-up"></span></button>
+					<a href = "ScriptIdeaMove.php?mode=0&id=<?php echo $Reader["ID"];?>"  class="btn btn-primary btn-success active">Valider</a>
+					<a href = "ScriptIdeaMove.php?mode=1&id=<?php echo $Reader["ID"];?>" class="btn btn-primary btn-warning active">Suppr</a>
+
+					
 
 				</div>
-
-				<div>
-
-					<a href = "ScriptIdeaMove.php?mode=0&id=<?php echo $Reader["ID"];?>"  class="Idea_Valider">Valider</a>
-					<a href = "ScriptIdeaMove.php?mode=1&id=<?php echo $Reader["ID"];?>" class="Idea_Suppr" style="background-color: red;">Suppr</a>
-
 				</div>
-				
 			</div>
+		</div>
+
 	<?php  
 		}
 
