17cb1be1f34f267c82bcdcb986e3407ce0cfe6c8
AAxx3 <yoamon51@live.fr>
2018-04-20 10:38:57 +0200

    Add files via upload

--- ProjetsWeb/accueil.php
+++ ProjetsWeb/accueil.php
@@ -35,10 +35,67 @@
 			<p>Bienvenue sur le site du BDE de l'Exia-Cesi de Reims</p>
 		</div>
 	</section>	
+	<div class="card mb-3" >	
+		<?php
+			$bdd = new PDO('mysql:host=localhost;dbname=phpweb;charset=utf8', 'root', '');
+
+			$requete = $bdd->query("SELECT Objet , Link , COUNT(Objet) AS nbr FROM `like_evenement` INNER JOIN evenement ON like_evenement.ID_Evenement = evenement.ID_eve INNER JOIN images ON like_evenement.ID_Evenement = images.ID_Evenement GROUP BY Objet, Link   LIMIT 1");
+
+			$result = $requete->fetch();
+
+			if (!$result) 
+			{
+				 
+			}
+			else 
+			{
+			?>
+				
+			<img class="card-img-top" src="<?php echo "Image/".$result["Link"]; ?>"  alt="Card image cap">
+			<div class="card-body">
+				<p class="card-title" style="font-size: 35px; margin-top: 2%;">Top Event</p>
+				 <p class="card-text" style="font-size: 20px;"> <?php echo $result["Objet"]; ?>
+				 	<?php echo $result["nbr"]; ?> <span class="glyphicon glyphicon-thumbs-up"></span> 
+				</p>
+				<?php 
+
+			}
+		?>
+		</div>
+	</div>
+
+	<div class="card">	
+		
+		<?php
+
+			$requete = $bdd->query("SELECT Description, Objet, COUNT(Objet) AS nbr FROM `aime` INNER JOIN boite_idees ON aime.ID_Boite_idees = boite_idees.ID GROUP BY Objet, description LIMIT 1");
+
+			$result = $requete->fetch();
+			if (!$result) 
+			{
+				echo "Aucune idée aimé ";
+			}
+			else 
+			{
+			?>
+			<div class="card-body">
+				<p class="card-title" style="font-size: 35px;">Top Idée</p>
+
+				<p class="card-text" style="font-size: 25px;"> <?php echo $result["Objet"]; ?> </br>
+				<?php echo $result["Description"]; ?>
+				<p><?php echo $result["nbr"]; ?> <span class="glyphicon glyphicon-thumbs-up"></span></p>
+				   
+				<?php 
+			}
+		?>
+		</div>
+	</div>
+
+
 	<footer>
 		<?php
-		include('footer.php');
-?>
+			include('footer.php');
+		?>
 	</footer>
 
 
--- ProjetsWeb/index.php
+++ ProjetsWeb/index.php
@@ -27,32 +27,37 @@
 			<p>Bienvenue sur le site du BDE de l'Exia-Cesi de Reims</p>
 		</div>
 	</section>	
-	<div>	
-		<h4><p>Top Event</p></h4>
+	<div class="card mb-3" >	
 		<?php
 			$bdd = new PDO('mysql:host=localhost;dbname=phpweb;charset=utf8', 'root', '');
 
-			$requete = $bdd->query("SELECT Objet , Link , COUNT(Objet) AS nbr FROM `like_evenement` INNER JOIN evenement ON like_evenement.ID_Evenement = evenement.ID INNER JOIN images ON like_evenement.ID_Evenement = images.ID_Evenement GROUP BY Objet, Link   LIMIT 1");
+			$requete = $bdd->query("SELECT Objet , Link , COUNT(Objet) AS nbr FROM `like_evenement` INNER JOIN evenement ON like_evenement.ID_Evenement = evenement.ID_eve INNER JOIN images ON like_evenement.ID_Evenement = images.ID_Evenement GROUP BY Objet, Link   LIMIT 1");
 
 			$result = $requete->fetch();
 
 			if (!$result) 
 			{
-				echo "ça marche pas : " ; 
+				 
 			}
 			else 
 			{
 			?>
-				<p><?php echo $result["Objet"]; ?></p>
-				<img src="<?php echo "Image/".$result["Link"]; ?>" alt="Mountain View">
-				<p><?php echo $result["nbr"]; ?></p>
+				
+			<img class="card-img-top" src="<?php echo "Image/".$result["Link"]; ?>"  alt="Card image cap">
+			<div class="card-body">
+				<p class="card-title" style="font-size: 35px; margin-top: 2%;">Top Event</p>
+				 <p class="card-text" style="font-size: 20px;"> <?php echo $result["Objet"]; ?>
+				 	<?php echo $result["nbr"]; ?> <span class="glyphicon glyphicon-thumbs-up"></span> 
+				</p>
 				<?php 
+
 			}
 		?>
+		</div>
 	</div>
 
-	<div>	
-		<h4><p>Top Idée</p></h4>
+	<div class="card">	
+		
 		<?php
 
 			$requete = $bdd->query("SELECT Description, Objet, COUNT(Objet) AS nbr FROM `aime` INNER JOIN boite_idees ON aime.ID_Boite_idees = boite_idees.ID GROUP BY Objet, description LIMIT 1");
@@ -65,13 +70,19 @@
 			else 
 			{
 			?>
-				<p><?php echo $result["Objet"]; ?></p>
-				<p><?php echo $result["Description"]; ?></p>
-				<p><?php echo $result["nbr"]; ?></p>
+			<div class="card-body">
+				<p class="card-title" style="font-size: 35px;">Top Idée</p>
+
+				<p class="card-text" style="font-size: 25px;"> <?php echo $result["Objet"]; ?> </br>
+				<?php echo $result["Description"]; ?>
+				<p><?php echo $result["nbr"]; ?> <span class="glyphicon glyphicon-thumbs-up"></span></p>
+				   
 				<?php 
 			}
 		?>
+		</div>
 	</div>
+
 
 	<footer>
 		<?php
